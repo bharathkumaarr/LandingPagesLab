fc917010e38fc6af3f4f8339ff376b9c08aa5677
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-08-28 18:27:54 +0530

    changes made to links in home page

--- 07k72.ca/src/components/home/HomeBottomText.jsx
+++ 07k72.ca/src/components/home/HomeBottomText.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 const HomeBottomText = () => {
   return (
-    <div className='font-[font2] flex items-center justify-center gap-2'>
-      <Link className='text-[6.5vw] leading-[5.5vw] border-5 border-white rounded-full px-10 pt-5 uppercase'>Projects</Link>
-      <Link className='text-[6.5vw] leading-[5.5vw] border-5 border-white rounded-full px-10 pt-5 uppercase'>Agence</Link>
+    <div className='font-[font2] flex items-center justify-center gap-2 '>
+      <Link to='/projects' className='hover:border-[#D3FD50] hover:text-[#D3FD50] text-[6.5vw] leading-[5.5vw] border-5 border-white rounded-full px-14 pt-5 uppercase'>Projects</Link>
+      <Link to='/agence' className='hover:border-[#D3FD50] hover:text-[#D3FD50] text-[6.5vw] leading-[5.5vw] border-5 border-white rounded-full px-14 pt-5 uppercase'>Agence</Link>
 
     </div>
   )
