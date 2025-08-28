e6b5d00f8dd92e9f7d704445a6ec75062dd5cc81
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-08-28 17:42:13 +0530

    router dom setup

--- 07k72.ca/src/App.jsx
+++ 07k72.ca/src/App.jsx
@@ -1,9 +1,25 @@
 
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
 
 function App() {
   return (
-    <div className='text-6xl'>app</div>
+    <div className='text-white'>
+      {/* <Link className='text-blue-400 mr-10 underline' to='/agence'>Agence</Link>
+      <Link className='text-blue-400 mr-10 underline' to='/'>Home</Link>
+      <Link className='text-blue-400 mr-10 underline' to='/projects'>Projects</Link> */}
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/agence' element={<Agence />} />
+      <Route path='/projects' element={<Projects />} />
+
+    </Routes>
+
+    </div>
   )
 }
 
