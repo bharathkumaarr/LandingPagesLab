5599760742978917d96f4c39a1dfeda7cc89b237
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-08-29 13:46:23 +0530

    agence hero1

--- 07k72.ca/src/components/home/Video.jsx
+++ 07k72.ca/src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756383500~exp=1756387100~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=f9077bbf0f4b6e0b01176c6d0606cc6010151a31acbe146a8d930d0ebafe231c&r=dXMtd2VzdDE%3D"></video>
+        <video className='h-full w-full object-cover' autoPlay loop muted src="https://player.vimeo.com/progressive_redirect/playback/899938964/rendition/1080p/file.mp4?loc=external&log_user=0&signature=6a2cb0320d09d02dcfdfa2ad504c89a510e6d93d55e9060b1b55d680fa2e2be9"></video>
     </div>
   )
 }
